f9fb48e3cb23445c921d12337856d446c209e092
Dev-JeromeBaek <superior051@icloud.com>
2018-09-02 17:59:21 +0900

    clean github page

--- admin/src/pages/Dashboard.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import DashboardCardList from '../components/dashboard/DashboardCardList';
-
-export default class Dashboard extends Component {
-  render() {
-    return <DashboardCardList />;
-  }
-}
--- admin/src/pages/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Dashboard } from './Dashboard';
-export { default as Api } from './Api';
-export { default as Err } from './Err';
